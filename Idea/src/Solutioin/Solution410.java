8cfb9d5b75d5c05b092e50a14b68c527e88f8d11
CBIB_CS <chen_199811@qq.com>
2021-03-01 11:09:12 +0800

    Merge remote-tracking branch 'origin/master'

--- Idea/src/Solutioin/Solution410.java
+++ Idea/src/Solutioin/Solution410.java
@@ -22,8 +22,22 @@ package Solutioin;
 public class Solution410 {
 
     public int splitArray(int[] nums, int m) {
-        int ans = 0;
-        return ans;
+        int left = 0 , right = 0;
+        for (int i = 0 ; i < nums.length ; i++ ) {
+            right += nums[i];
+            if (left < nums[i]) {
+                left = nums[i];
+            }
+            while (left < right) {
+                int mid = (right - left) / 2 + left;
+                if(check(nums , mid , m)){
+                    right = mid;
+                } else {
+                    left = mid + 1;
+                }
+            }
+        }
+        return left;
     }
 
     public boolean check(int[] nums , int x , int m) {
