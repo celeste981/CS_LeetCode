d77d32a4170371e2926740625a504739381b3178
chensi <chen_199811@qq.com>
2021-03-01 08:45:57 +0800

    2021 02 26

---
+++ Idea/src/Solutioin/Solution1178.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright (C), 2016-2021, 华中科技大学
+ * FileName: Solution1178
+ * Author:   CS
+ * Date:     2021/2/26 16:54
+ * Description: 猜字谜
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package Solutioin;
+
+import java.util.List;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈猜字谜〉
+ *
+ * @author CS
+ * @create 2021/2/26
+ * @since 1.0.0
+ */
+public class Solution1178 {
+//    public List<Integer> findNumOfValidWords(String[] words, String[] puzzles) {
+//
+//    }
+}
---
+++ Idea/src/Solutioin/Solution410.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (C), 2016-2021, 华中科技大学
+ * FileName: Solution410
+ * Author:   CS
+ * Date:     2021/2/28 23:24
+ * Description: 分割数组的最大值
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package Solutioin;
+
+import Solutioin.Recursion.Solution78;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈分割数组的最大值〉
+ *
+ * @author CS
+ * @create 2021/2/28
+ * @since 1.0.0
+ */
+public class Solution410 {
+
+    public int splitArray(int[] nums, int m) {
+        int ans = 0;
+        return ans;
+    }
+    public static void main(String[] args) {
+        Solution410 solution410 = new Solution410();
+        System.out.println(solution410.splitArray(new int[]{7,2,5,10,8} , 2));
+    }
+}
